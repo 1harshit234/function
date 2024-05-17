b34473b335e7761f14c76ca7bc70eecdb9bfcbd1
Harshit Mishra <165307122+1harshit234@users.noreply.github.com>
2024-05-17 03:31:53 -0700

    Add files via upload

---
+++ checkrange.py
@@ -0,0 +1,15 @@
+def f(n):
+  for x in range ( 1,25,1):
+
+    if n==x :
+        print("tr")
+        break
+    else :
+        print("",x)
+n = int(input("write the no"))
+f(n)
+     
+#write the programm in which we have to define a range and we have to co,mpare that the number lie within the range or not
+
+
+
