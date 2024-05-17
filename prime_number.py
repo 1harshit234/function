649c273a4093781c5523a058e9c0793017215890
Harshit Mishra <165307122+1harshit234@users.noreply.github.com>
2024-05-17 03:28:29 -0700

    Add files via upload

---
+++ prime_number.py
@@ -0,0 +1,9 @@
+def x(number):
+    for i in range(2,number+1):
+          if (number % i == 0):
+               print("not prime")
+          else:
+               continue
+            
+x(31)           
+            
